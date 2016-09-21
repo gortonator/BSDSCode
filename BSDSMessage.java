8026b2d329c7b0c67eec26b4b1430518821bdbb4
gortonator <gortonator@outlook.com>
2016-09-21 14:29:53 -0700

    Initial commit
     Changes to be committed:
    	new file:   BSDSContent.java
    	new file:   BSDSMessage.java
    	new file:   BSDSPublishInterface.java
    	new file:   BSDSSubscribeInterface.java
    	new file:   CAPubClient.java
    	new file:   CAServer.java
    	new file:   CASubClient.java

---
+++ BSDSMessage.java
@@ -0,0 +1,35 @@
+/**
+ * Created by Ian Gortan on 9/20/2016.
+ */
+package BSDSAssignment1;
+
+public class BSDSMessage {
+    private String title;
+    private String message;
+    private String source;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+}
+
